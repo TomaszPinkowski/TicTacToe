f2f577ae75de7027bffe5a290ef379edd3600e69
TomPin <tompin6835@gmail.com>
2020-02-10 18:22:29 +0100

    adding game model

---
+++ src/main/java/pl/tpinkowski/tictactoe/model/Board.java
@@ -0,0 +1,5 @@
+package pl.tpinkowski.tictactoe.model;
+
+public class Board {
+    Field[][] fields;
+}
---
+++ src/main/java/pl/tpinkowski/tictactoe/model/Game.java
@@ -0,0 +1,8 @@
+package pl.tpinkowski.tictactoe.model;
+
+public class Game {
+    Player player1;
+    Player player2;
+
+    GameState gameState;
+}
